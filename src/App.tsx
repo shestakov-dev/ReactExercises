f3bfa491339a91442282f8ec01b9ab1b65578c17
Alexander Shestakov <alexandurshestakov@gmail.com>
2026-02-26 14:56:25 +0200

    initialized the app

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { RouterProvider } from "@tanstack/react-router";
+import "./App.css";
+import { router } from "./router";
+
+function App() {
+	return <RouterProvider router={router} />;
+}
+
+export default App;
